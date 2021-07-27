feae227b6438414366fda1a17cc69d240edc52d0
Markus Moltke <makakwastaken@gmail.com>
2021-07-27 13:43:52 +0200

    Fixed eslint errors

--- src/strategy.js
+++ src/strategy.js
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
